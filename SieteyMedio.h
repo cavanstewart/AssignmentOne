64a074ae4ec359699a176ed182eb91618a0aedd4
Benjamin Dang <rm>
2017-01-28 00:50:43 -0800

    added header and companion log file for the game

---
+++ SieteyMedio.h
@@ -0,0 +1,13 @@
+//
+//  SieteyMedio.h
+//  AssignmentOne
+//
+//  Created by Benjamin Dang on 1/28/17.
+//  Copyright © 2017 Benjamin Dang. All rights reserved.
+//
+
+#ifndef SieteyMedio_h
+#define SieteyMedio_h
+
+
+#endif /* SieteyMedio_h */
